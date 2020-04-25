3b957d76e6abe09600b0c3f0105d734ad4bf2dde
root <root@test.schaffner.org>
2020-04-25 08:12:28 +0000

    Initial Release

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-cp shoutcast2.php /usr/local/cwpsrv/htdocs/resources/admin/modules/
-useradd -m shoutcast2
-cd /home/shoutcast2
+cp icecast.php /usr/local/cwpsrv/htdocs/resources/admin/modules/
+useradd -m icecast
+cd /home/icecast
 wget http://download.nullsoft.com/shoutcast/tools/sc_serv2_linux_x64-latest.tar.gz
 tar -xzf sc_serv2_linux_x64-latest.tar.gz
 cat <<'EOF' >> /usr/local/cwpsrv/htdocs/resources/admin/include/3rdparty.php
 <noscript>
 </ul>
 <li class="custom-menu"> <!-- this class "custom-menu" was added so you can remove the Developer Menu easily if you want -->
-    <a href="?module=shoutcast2"><span class="icon16 icomoon-icon-volume-high"></span>ShoutCast 2</a>
+    <a href="?module=icecast"><span class="icon16 icomoon-icon-volume-high"></span>Icecast</a>
 </li>
 <li style="display:none;"><ul>
 </noscript>
@@ -16,7 +16,7 @@ cat <<'EOF' >> /usr/local/cwpsrv/htdocs/resources/admin/include/3rdparty.php
         $(document).ready(function() {
                 var newButtons = ''
                 +' <li>'
-                +' <a href="?module=shoutcast2" class=""><span aria-hidden="true" class="icon16 icomoon-icon-volume-high"></span>ShoutCast 2</a>'
+                +' <a href="?module=icecast" class=""><span aria-hidden="true" class="icon16 icomoon-icon-volume-high"></span>Icecast</a>'
                 +'</li>';
                 $("li#mn-3").before(newButtons);
         });
